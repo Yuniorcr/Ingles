da18c8071354276f65b4c7cba61f8b6b63e2ea98
Clinton Cardenas <46506844+Yuniorcr@users.noreply.github.com>
2021-01-31 09:30:44 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="https://www.facebook.com/Tejedoras-102991311763329/community/" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <img src="assets/icons/social-facebook.png" alt="facebook-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Whatsapp">
+            <img src="assets/icons/whatsapp.png" alt="googleplus-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Telegram">
+            <img src="assets/icons/telegrama.png" alt="linkedin-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Instagram">
+            <img src="assets/icons/instagram.png" alt="pinterest-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <img src="assets/icons/social-twitter.png" alt="twitter-icon">
+        </a>
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">TEJEDORAS SAC. &copy; 2021</h5>
+</footer>
---
+++ library/configServer.php
@@ -0,0 +1,5 @@
+<?php
+define("USER", "root");
+define("SERVER", "34.66.155.216");
+define("BD", "store");
+define("PASS", "smith456");
